ead388882a8321a70e5e19b39caac6770e2e230c
Fellipe Guardivir <fellipe.guardivir@gmail.com>
2026-06-15 15:31:05 -0300

    verificação de letras

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ let exibicao =
 document.querySelector(".forca")
 .innerText =
 exibicao.join(" ");
+
+function jogar(){
+
+    let letra =
+    document
+    .getElementById("letra")
+    .value
+    .toUpperCase();
+
+    for(let i=0;i<palavra.length;i++){
+
+        if(palavra[i]===letra){
+
+            exibicao[i]=letra;
+        }
+    }
+
+    document.querySelector(".forca")
+    .innerText =
+    exibicao.join(" ");
+}
